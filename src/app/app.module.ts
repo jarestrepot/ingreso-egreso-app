2fc68842e04169da6ce6c59174f6d01e473f961d
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-05-20 12:48:31 +0200

    Store - Initial Store Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireModule } from '@angular/fire/compat';
+
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from './store/app.reducer';
+
+// Custom document firebase
+import { firebaseProviderFrom } from './firebase.config';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from '@auth/auth.module';
-import { firebaseProviderFrom } from './firebase.config';
 import { environments } from 'src/environments/environmets';
 
 @NgModule({
@@ -14,9 +20,11 @@ import { environments } from 'src/environments/environmets';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot(appReducers),
     AppRoutingModule,
     AuthModule,
-    AngularFireModule.initializeApp(environments.firebase) // injection NullErrorInjection
+    AngularFireModule.initializeApp(environments.firebase),
+    StoreModule.forRoot({}, {}) // injection NullErrorInjection
   ],
   providers: [
     firebaseProviderFrom
---
+++ src/app/store/app.reducer.ts
@@ -0,0 +1,15 @@
+import { ActionReducerMap } from '@ngrx/store';
+import * as UI from '@shared/ui.reducer';
+
+// Aplication state
+export interface AppState {
+  ui: UI.State
+}
+
+
+// Reducers
+export const appReducers: ActionReducerMap<AppState> = {
+  ui: UI.uiReducer,
+  
+}
+
